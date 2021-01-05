2684da5f4382a381dc69e4883a5caac0cbc00056
Kurt Lekanger <kurt@lekanger.no>
2021-01-05 19:56:51 +0100

    Remove: Unused import

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
-import { useLocation } from "@reach/router";
+/* import { useLocation } from "@reach/router"; */
 import { useStaticQuery, graphql } from "gatsby";
 
 const SEO = ({ title, description }) => {
