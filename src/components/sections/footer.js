f96a681f4411cedd1621f9a0d3bdb40d71602596
Kurt Lekanger <kurt@lekanger.no>
2021-01-17 10:15:23 +0100

    Add: Footer info

--- src/components/sections/footer.js
+++ src/components/sections/footer.js
@@ -1,9 +1,49 @@
 import React from "react";
+import { Box, Flex, Text, Link } from "@chakra-ui/react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>
-      <h1>This is the footer</h1>
-    </div>
+    <>
+      <hr />
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        w="100%"
+        mb={4}
+        py={3}
+        px={10}
+        color="gray.700"
+        fontSize="sm"
+        direction={["column", "column", "row", "row"]}
+      >
+        <Text mb={[4, 4, 0, 0]}>
+          &copy; {currentYear} Boligsameiet Gartnerihagen{" "}
+        </Text>
+        <Text mb={[4, 4, 0, 0]}>
+          Epost:{" "}
+          <Link
+            href="mailto:gartnerihagen@gmail.com"
+            _hover={{ textDecor: "none", color: "blue.500" }}
+          >
+            gartnerihagen@gmail.com
+          </Link>{" "}
+          <br />
+          Telefon: +47 - 4021 0140{" "}
+        </Text>
+        <Text mb={[4, 4, 0, 0]}>
+          Webdesign:{" "}
+          <Link
+            href="https://www.lekanger.no"
+            _hover={{ textDecor: "none", color: "blue.500" }}
+          >
+            Kurt Lekanger
+          </Link>
+        </Text>
+      </Flex>
+    </>
   );
 }
