9dd93d8018dc576f4f578059b181c968ae467c88
Kurt Lekanger <kurt@lekanger.no>
2021-02-02 18:39:27 +0100

    Customize menu buttons.

--- src/components/private-components/privateServicebox.js
+++ src/components/private-components/privateServicebox.js
@@ -5,9 +5,16 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { Box, Stack, Text } from "@chakra-ui/react";
 
-const MyBox = ({ children }) => {
+// Define custom servicebox buttons as clickable links
+// Button title and link URL are fetched from Contentful
+// The first four buttons link to one document per button
+// The two last buttons (5 and 6) are multi document buttons and should go to pages
+// fetching all documents for menu5files and menu6files
+
+const MenuButton = (props) => {
   return (
     <Box
+      as="a"
       w={["95vw", "95vw", "30%", "30%"]}
       h={["4rem", "6rem"]}
       p={3}
@@ -20,6 +27,9 @@ const MyBox = ({ children }) => {
       textColor="gray.200"
       fontSize={["md", "2xl", "2xl", "2xl"]}
       fontWeight="600"
+      href={props.linkTo || null}
+      target="_blank"
+      rel="noopener noreferrer"
       _hover={{
         transform: "scale(1.02)",
         transitionDuration: "0.1s",
@@ -29,12 +39,14 @@ const MyBox = ({ children }) => {
         transform: "scale(1.00)",
       }}
     >
-      {children}
+      {props.children}
     </Box>
   );
 };
 
 export default function PrivateServiceBox() {
+  // Get text and links for menu items from Contentful
+  // In Contentful, the field serviceMenuTitle should equal "infomeny"
   const data = useStaticQuery(graphql`
     query MyQuery {
       menuItems: contentfulServiceMenu(serviceMenuTitle: { eq: "infomeny" }) {
@@ -89,31 +101,31 @@ export default function PrivateServiceBox() {
         pb={0}
         my={["2", "2", "4", "4"]}
       >
-        <MyBox>
+        <MenuButton linkTo={menuItems.menu1File.file.url}>
           <Text textAlign="center">{menuItems.menu1}</Text>
-        </MyBox>
+        </MenuButton>
 
-        <MyBox>
+        <MenuButton linkTo={menuItems.menu2File.file.url}>
           <Text textAlign="center">{menuItems.menu2}</Text>
-        </MyBox>
-        <MyBox>
+        </MenuButton>
+        <MenuButton linkTo={menuItems.menu3File.file.url}>
           <Text textAlign="center">{menuItems.menu3}</Text>
-        </MyBox>
+        </MenuButton>
       </Stack>
       <Stack
         direction={["column", "column", "row", "row"]}
         justify="space-between"
         pt={0}
       >
-        <MyBox>
+        <MenuButton linkTo={menuItems.menu4File.file.url}>
           <Text textAlign="center">{menuItems.menu4}</Text>
-        </MyBox>
-        <MyBox>
+        </MenuButton>
+        <MenuButton>
           <Text textAlign="center">{menuItems.menu5}</Text>
-        </MyBox>
-        <MyBox>
+        </MenuButton>
+        <MenuButton>
           <Text textAlign="center">{menuItems.menu6}</Text>
-        </MyBox>
+        </MenuButton>
       </Stack>
     </Box>
   );
