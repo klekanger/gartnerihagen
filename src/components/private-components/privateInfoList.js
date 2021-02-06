14fe10b31c2ca8c6af459027b03c2198b4721299
Kurt Lekanger <kurt@lekanger.no>
2021-02-06 12:30:27 +0100

    Change layout of list of info-articles

--- src/components/private-components/privateInfoList.js
+++ src/components/private-components/privateInfoList.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, useStaticQuery, Link as GatsbyLink } from "gatsby";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 import GatsbyImage from "gatsby-image";
 
-import { Box, Image, Heading, Text } from "@chakra-ui/react";
+import {
+  Stack,
+  Hstack,
+  Flex,
+  Container,
+  Box,
+  Image,
+  Link,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 
 const PrivateInfoList = () => {
   const data = useStaticQuery(graphql`
@@ -33,6 +44,73 @@ const PrivateInfoList = () => {
   `);
 
   const postNodes = data.posts.nodes || [];
+
+  return (
+    <Box textAlign="left" pt={4} mb={8} mt={6}>
+      <Heading as="h1" size="2xl">
+        Informasjon fra styret
+      </Heading>
+      <Text as="p" mb={["3rem", "3rem", "6rem", "6rem"]}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at
+        massa hendrerit, ultrices justo quis, vestibulum erat.
+      </Text>
+
+      <Box>
+        {postNodes.map((post) => (
+          <Flex
+            key={post.contentful_id}
+            direction={["column-reverse", "column-reverse", "row", "row"]}
+          >
+            <Box ml={0} mb={[4, 4, 0, 0]} pr={8} w="90%">
+              <Heading as="h1" size="xl" mb={4} my={[]} textAlign="left">
+                {post.title}
+              </Heading>
+              <Text fontSize="md" textAlign="left">
+                {post.excerpt.excerpt}
+              </Text>
+              <Text
+                textAlign="left"
+                pt={3}
+                pb={[6, 6, 3, 3]}
+                fontSize={{ base: "xs", sm: "sm", md: "lg" }}
+                _hover={{ color: "primary" }}
+              >
+                <Link
+                  as={GatsbyLink}
+                  to="#"
+                  color="black"
+                  _hover={{ textDecor: "none" }}
+                >
+                  Les mer <ChevronRightIcon />
+                </Link>
+              </Text>
+            </Box>
+            <Image
+              as={GatsbyImage}
+              fluid={post.featuredImage.fluid}
+              shadow="lg"
+              mb={[2, 2, 8, 8]}
+              w="100%"
+              maxH={["50vh", "50vh", "40vh", "40vh"]}
+              alt={post.featuredImage.description}
+            />
+          </Flex>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default PrivateInfoList;
+
+/*
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import GatsbyImage from "gatsby-image";
+
+import { Box, Image, Heading, Text } from "@chakra-ui/react";
+
+
 
   return (
     <Box textAlign="left" pt={4} mb={8} mt={6}>
@@ -73,3 +151,4 @@ const PrivateInfoList = () => {
 };
 
 export default PrivateInfoList;
+*/
