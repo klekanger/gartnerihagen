5e70a3fd2d23df636c21ff49e7d3293458b18995
Kurt Lekanger <kurt@lekanger.no>
2021-02-08 15:52:52 +0100

    Add: custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layouts/layout';
 
 // styles
 const pageStyles = {
@@ -16,38 +17,28 @@ const headingStyles = {
 const paragraphStyles = {
   marginBottom: 48,
 };
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{' '}
-        <span role='img' aria-label='Pensive emoji'>
-          😔
-        </span>{' '}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to='/'>Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <main style={pageStyles}>
+        <title>Not found</title>
+        <h1 style={headingStyles}>404 - siden eksisterer ikke</h1>
+        <p style={paragraphStyles}>
+          Beklager... Vi fant ikke det du lette etter. Det er antagelig vår
+          skyld, ikke din.{' '}
+          <span role='img' aria-label='Pensive emoji'>
+            😔
+          </span>{' '}
+          <br />
+          <br />
+          <Link to='/'>
+            <b>Gå tilbake til forsiden ></b>
+          </Link>
+        </p>
+      </main>
+    </Layout>
   );
 };
 
