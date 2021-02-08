0e279680115e1a97087df4ec6e30913e08d94601
Kurt Lekanger <kurt@lekanger.no>
2021-02-08 19:36:14 +0100

    Styling

--- src/components/sections/hero-wide.js
+++ src/components/sections/hero-wide.js
@@ -64,7 +64,7 @@ const HeroWide = () => {
         bgColor='darkTransparent'
         w={['90vw', '90vw', '80vw', '60vw']} // Set height of transparent background drop to height of text/heading container
         position='absolute'
-        top={[180, 180, 200, 200]}
+        top={[150, 150, 180, 180]}
         left={[4, 4, 16, 16]}
         overflow='auto'
       >
