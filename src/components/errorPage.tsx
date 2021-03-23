73d9f50a75d53fb4e8d4b6c250a9dc4001615258
Kurt Lekanger <kurt@lekanger.no>
2021-03-23 14:42:26 +0100

    Convert to typescript

---
+++ src/components/errorPage.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+function ErrorPage({ errorMsg = 'Noe gikk galt' }: { errorMsg: string }) {
+  return (
+    <Box w='95vw' ml='0' pr={['0', '0', '5vw', '25vw']} pt={8} pb={16}>
+      <Box pt={['8', '8', '16', '16']} textAlign='left'>
+        <Heading as='h1' size='3xl' textColor='black' pb={16}>
+          {errorMsg}
+        </Heading>
+      </Box>
+    </Box>
+  );
+}
+
+export default ErrorPage;
