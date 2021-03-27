29c1c808904c0668d1dc1536695ae1c1d756030e
Kurt Lekanger <kurt@lekanger.no>
2021-03-27 11:43:40 +0100

    refactor: Use standard Article component

--- src/components/private-components/privateInfo.tsx
+++ src/components/private-components/privateInfo.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import { Link as GatsbyLink } from 'gatsby';
 import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
 import { Box, Image, Heading, Text, Button } from '@chakra-ui/react';
-import { renderRichText } from 'gatsby-source-contentful/rich-text';
-import renderRichTextOptions from '../../theme/renderRichTextOptions';
+import Article from '../article';
 import { format, parseISO } from 'date-fns';
 import norwegian from 'date-fns/locale/nb';
 
@@ -81,49 +79,12 @@ export default function PrivateInfo({ slug, postData }: IPrivateInfoProps) {
   ) : null;
 
   return (
-    <Box maxWidth={['97%', '95%', '95%', '70%']} pt={[8, 12, 16, 24]}>
-      <Heading
-        as='h1'
-        fontSize={['4xl', '6xl', '6xl', '7xl']}
-        textAlign={['center', 'left', 'left', 'left']}
-        pb={2}
-      >
-        {title}
-      </Heading>
-      {topImage}
-      <Text
-        as='p'
-        textAlign='left'
-        ml={2}
-        p={2}
-        fontSize={['sm', 'sm', 'sm', 'md']}
-      >
-        <em>{featuredImage.description}</em>
-      </Text>
-      <Text as='div' my={[5, 10, 10, 10]}>
-        {renderRichText(bodyText, renderRichTextOptions)}
-      </Text>
-
-      <Text
-        fontSize={['sm', 'sm', 'sm', 'sm']}
-        fontStyle='italic'
-        pb={[4, 16]}
-        textAlign='left'
-      >
-        {publishDate}
-      </Text>
-
-      <Box align='left'>
-        <Button
-          as={GatsbyLink}
-          to={`/informasjon/`}
-          variant='standard'
-          mb={16}
-          _hover={{ textDecor: 'none' }}
-        >
-          Gå tilbake
-        </Button>
-      </Box>
-    </Box>
+    <Article
+      title={title}
+      bodyText={bodyText}
+      createdAt={createdAt}
+      updatedAt={updatedAt}
+      mainImage={featuredImage}
+    />
   );
 }
