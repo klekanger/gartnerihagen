7cc551490b243e9059c93f6534bd1325ffd8c4cb
Kurt Lekanger <kurt@lekanger.no>
2021-03-30 22:29:53 +0200

    chore: Bump versions of gatsby, chakra.

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /images/, // See below to configure properly
+          include: /images/,
         },
       },
     },
