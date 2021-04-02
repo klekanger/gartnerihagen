0b4d9ff56faec2921901a4ea09edb387365427d2
Kurt Lekanger <kurt@lekanger.no>
2021-04-02 18:37:41 +0200

    feat: Show authors in articles

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -11,6 +11,10 @@ interface IArticleProps {
     description: string;
   };
   title: string;
+  author?: {
+    firstName: string;
+    lastName: string;
+  }[];
   bodyText: any;
   createdAt: string;
   updatedAt: string;
@@ -20,15 +24,31 @@ interface IArticleProps {
 function Article({
   mainImage,
   title,
+  author,
   bodyText,
   createdAt,
   updatedAt,
   isPage,
 }: IArticleProps) {
-  const publishDate =
+  const publishDate: string =
     createdAt !== updatedAt
       ? `Publisert: ${createdAt} (oppdatert: ${updatedAt})`
       : `Publisert: ${createdAt}`;
+
+  // Make string that will be used to show
+  // a list of authors (with comma separators if there are more than one)
+  let authorsToShow: string;
+  author ? (authorsToShow = 'Av: ') : '';
+  // Skip if author is not passed as props to this component
+  if (author) {
+    author.forEach((el, index) => {
+      if (index < author.length - 1 && author.length !== 1) {
+        authorsToShow = authorsToShow + el.firstName + ' ' + el.lastName + ', ';
+      } else {
+        authorsToShow = authorsToShow + el.firstName + ' ' + el.lastName;
+      }
+    });
+  }
 
   return (
     <Box maxWidth={['97%', '95%', '95%', '70%']} pt={[12, 16, 16, 24]}>
@@ -71,6 +91,8 @@ function Article({
         textAlign='left'
       >
         {publishDate}
+        <br />
+        {authorsToShow}
       </Text>
       <Box align='left'>
         <Button
