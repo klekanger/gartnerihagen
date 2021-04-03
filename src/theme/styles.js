0a4a86bb5453ee2a7c89ce0f9bada1f4094904dd
Kurt Lekanger <kurt@lekanger.no>
2021-04-03 09:57:14 +0200

    chore. Remove console.log

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -1,13 +1,10 @@
 const styles = {
-  global: (props) => {
-    console.log('props: ', props);
-    return {
-      body: {
-        bg: '#F6F8FF',
-        lineHeight: 'base',
-      },
-    };
-  },
+  global: (props) => ({
+    body: {
+      bg: '#F6F8FF',
+      lineHeight: 'base',
+    },
+  }),
 };
 
 export default styles;
