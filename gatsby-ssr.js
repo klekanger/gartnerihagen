764b1f6e22358c5736083961665c84dcbc06f10b
Kurt Lekanger <kurt@lekanger.no>
2021-04-07 18:30:16 +0200

    feat: Start implementing Auth0

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import { wrapPageElement as wrap } from './src/chakra-wrapper';
-import IdentityProvider from './src/context/identity-context';
+import { Auth0Provider } from '@auth0/auth0-react';
+// import IdentityProvider from './src/context/identity-context';
+import { navigate } from 'gatsby';
+
+const onRedirectCallback = (appState) => {
+  // Use Gatsby's navigate method to replace the url
+  navigate(appState?.returnTo || '/', { replace: true });
+};
 
 export const wrapRootElement = ({ element }) => (
-  <IdentityProvider>{element}</IdentityProvider>
+  <Auth0Provider
+    domain={process.env.AUTH0_DOMAIN}
+    clientId={process.env.AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    {element}
+  </Auth0Provider>
 );
 
 export const wrapPageElement = wrap;
