f0363698f8c89f051c520390e613504fbcb1ddd1
Kurt Lekanger <kurt@lekanger.no>
2021-04-10 12:31:19 +0200

    fix: bugfix for login

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -20,9 +20,3 @@ export const wrapRootElement = ({ element }) => (
 );
 
 export const wrapPageElement = wrap;
-
-// TODO
-//
-// Auth0 - protecting a route in a Gatsby app:
-// https://github.com/auth0/auth0-react/blob/master/EXAMPLES.md#2-protecting-a-route-in-a-gatsby-app
-//
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,22 @@
+import * as React from 'react';
 import { wrapPageElement as wrap } from './src/chakra-wrapper';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { navigate } from 'gatsby';
+
+const onRedirectCallback = (appState) => {
+  // Use Gatsby's navigate method to replace the url
+  navigate(appState?.returnTo || '/', { replace: true });
+};
+
+export const wrapRootElement = ({ element }) => (
+  <Auth0Provider
+    domain={process.env.AUTH0_DOMAIN}
+    clientId={process.env.AUTH0_CLIENT_ID}
+    redirectUri={process.env.AUTH0_CALLBACK}
+    onRedirectCallback={onRedirectCallback}
+  >
+    {element}
+  </Auth0Provider>
+);
 
 export const wrapPageElement = wrap;
