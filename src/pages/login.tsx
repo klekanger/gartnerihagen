b66f0340f41e9275504491f79986926bc777085f
Kurt Lekanger <kurt@lekanger.no>
2021-04-24 08:26:03 +0200

    feat: Login page with redirects

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,38 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import { navigate } from 'gatsby';
+import LoadingSpinner from '../components/loading-spinner';
+import { Box, Heading, Text } from '@chakra-ui/react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-import LoadingSpinner from '../components/loading-spinner';
-
 const Login = () => {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const { isLoading, isAuthenticated, error, loginWithRedirect } = useAuth0();
 
-  if (!isAuthenticated) {
-    loginWithRedirect();
+  if (isLoading) {
     return <LoadingSpinner />;
   }
 
-  return <h1>...</h1>;
+  if (error) {
+    return <div>Det har oppstått en feil... {error.message}</div>;
+  }
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    } else {
+      navigate('/min-side');
+    }
+  }, []);
+
+  return (
+    <Box h='70vh' pt='10vh'>
+      <Heading as='h1'>
+        Hvis du ikke er logget inn, vil du bli sendt videre til
+        innloggingssiden.
+      </Heading>
+      <Text as='p'>Ved problemer, ta kontakt med styret.</Text>
+    </Box>
+  );
 };
 
 export default Login;
