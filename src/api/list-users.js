330d1566ca578161fed6e0a0d3f72bb3510cc0a4
Kurt Lekanger <kurt@lekanger.no>
2021-06-12 19:53:41 +0200

    feat: Continue working on user admin solution

--- src/api/list-users.js
+++ src/api/list-users.js
@@ -1,6 +1,61 @@
 const ManagementClient = require('auth0').ManagementClient;
+const {
+  JwtVerifier,
+  JwtVerifierError,
+  claimToArray,
+  getTokenFromHeader,
+} = require('@serverless-jwt/jwt-verifier');
+
+const jwt = new JwtVerifier({
+  issuer: `https://${process.env.AUTH0_BACKEND_DOMAIN}/`,
+  audience: `https://${process.env.AUTH0_USERADMIN_AUDIENCE}`,
+});
 
 export default async function handler(req, res) {
+  // Verifiser token mottatt fra frontend
+  const mustHavePermissions = [
+    'read:users',
+    'update:users',
+    'delete:users',
+    'create:users',
+  ];
+  let claims, permissions;
+  const token = getTokenFromHeader(req.get('authorization'));
+
+  // Verify access token received from frontend
+  try {
+    claims = await jwt.verifyAccessToken(token);
+    permissions = claims?.permissions ?? [];
+  } catch (err) {
+    if (err instanceof JwtVerifierError) {
+      return res.status(403).json({
+        error: `Something went wrong. ${err?.code}`,
+        error_description: `${err.message}`,
+      });
+    }
+  }
+
+  // check if user should have access at all
+  if (!claims || !claims?.scope) {
+    return res.status(403).json({
+      error: 'access denied',
+      error_description: 'You do not have access to this',
+    });
+  }
+
+  // Check the permissions
+  if (!claims?.permissions.includes('update:users')) {
+    return res.status(403).json({
+      error: 'no update access',
+      error_description:
+        'Du må ha admin-tilgang for å administrere brukere. Ta kontakt med styret.',
+      body: {
+        data: [],
+      },
+    });
+  }
+
+  // Get list of all users from Auth0 management API
   const auth0 = new ManagementClient({
     domain: `${process.env.AUTH0_BACKEND_DOMAIN}`,
     clientId: `${process.env.AUTH0_BACKEND_CLIENT_ID}`,
@@ -20,8 +75,8 @@ export default async function handler(req, res) {
     };
   }
 
-  res.json({
-    statusCode: 200,
+  // Success! Return a list of all users to client
+  return res.status(200).json({
     body: {
       data: userList,
     },
--- src/components/private-components/userAdminPage.tsx
+++ src/components/private-components/userAdminPage.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import LoadingSpinner from '../../components/loading-spinner';
+import NoAccess from '../../components/no-access';
 import { useAuth0 } from '@auth0/auth0-react';
 
 import {
@@ -14,6 +15,7 @@ import {
   Stack,
   Grid,
 } from '@chakra-ui/react';
+import { access } from 'fs/promises';
 
 export default function UserAdminPage() {
   const {
@@ -24,28 +26,43 @@ export default function UserAdminPage() {
     getAccessTokenSilently,
     getAccessTokenWithPopup,
   } = useAuth0();
-
-  const [myUsers, setUsers] = useState(null);
+  const [myUsers, setMyUsers] = useState(null);
+  const [noAccess, setNoAccess] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
+  let userList, sortedUsers;
 
   useEffect(() => {
     const opts = {
       audience: 'https://useradmin.gartnerihagen-askim.no',
     };
 
+    // TODO
+    // Have a look at this and refactor
+    // UseAPI hook for accessing protecting APIS with an access token
+    // https://github.com/auth0/auth0-react/blob/master/EXAMPLES.md#4-create-a-useapi-hook-for-accessing-protected-apis-with-an-access-token
+
     // Fetch all users from user admin API
     async function getUserData() {
       try {
         const accessToken = await getAccessTokenSilently(opts);
 
-        await fetch('/api/list-users', {
+        const response = await fetch('/api/list-users', {
           method: 'GET',
           headers: {
             Authorization: `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
           },
-        })
-          .then((response) => response.text())
-          .then((result) => setUsers(JSON.parse(result)));
+        });
+        const result = await response.json();
+
+        if (response?.status === 200) {
+          setMyUsers(result);
+        } else {
+          setNoAccess(true);
+          setMyUsers([]);
+          setErrorMsg(result?.error_description ?? null);
+          return;
+        }
       } catch (error) {
         console.error('ERROR: ', error);
         if (error.error === 'consent_required') {
@@ -56,7 +73,7 @@ export default function UserAdminPage() {
     getUserData();
   }, []);
 
-  if (isLoading || !myUsers) {
+  if (!myUsers) {
     return <LoadingSpinner spinnerMessage='Laster inn brukere' />;
   }
 
@@ -65,18 +82,111 @@ export default function UserAdminPage() {
   }
 
   // Sort users by name (case insensitive)
+  if (!noAccess) {
+    sortedUsers = myUsers.body.data.sort((a, b) => {
+      let nameA = a.name.toUpperCase();
+      let nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  } else {
+    sortedUsers = [];
+  }
 
-  const sortedUsers = myUsers.body.data.sort((a, b) => {
-    let nameA = a.name.toUpperCase();
-    let nameB = b.name.toUpperCase();
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1;
-    }
-    return 0;
-  });
+  // Conditional rendering of user list. Render "No access" warning if user does not have access
+  if (noAccess) {
+    userList = (
+      <NoAccess
+        errorTitle='Du har ikke tilgang til dette'
+        errorMsg={errorMsg}
+      />
+    );
+  } else {
+    userList = (
+      <Grid
+        templateColumns={{
+          sm: 'repeat(1, 1fr)',
+          md: 'repeat(2, 1fr)',
+          lg: 'repeat(2, 1fr)',
+          xl: 'repeat(3, 1fr)',
+        }}
+        pt={8}
+        gap={4}
+        mb={4}
+        mt={0}
+      >
+        {sortedUsers.map((user) => (
+          <Box
+            key={user?.user_id}
+            minH={64}
+            minW='100%'
+            borderWidth='1px'
+            rounded='md'
+            shadow='lg'
+            bgColor='#eee'
+            overflow='hidden'
+          >
+            <Flex
+              m={8}
+              direction={['column', 'column', 'row', 'row']}
+              align={['center', 'center', 'left', 'left']}
+            >
+              <Image
+                src={user?.picture}
+                alt={user?.name}
+                rounded='50%'
+                width={32}
+                mt={4}
+                mx={8}
+              />
+              <Text as='div' fontSize='lg' fontWeight='semibold' align='left'>
+                {user?.name}
+                {user?.app_metadata?.Role ? (
+                  <Badge colorScheme='red'>ADMIN</Badge>
+                ) : (
+                  ''
+                )}
+              </Text>
+            </Flex>
+
+            <Box mx={8} mb={8}>
+              <Stack
+                direction={['column', 'column', 'row', 'row']}
+                align='center'
+                justify='space-between'
+              >
+                <Button variant='standard' w='100%' p={8}>
+                  Endre bruker
+                </Button>
+                <Button variant='danger' w='100%' p={8}>
+                  Slett bruker
+                </Button>
+              </Stack>
+              <Stack
+                direction={['column', 'column', 'row', 'row']}
+                my={[2, 2, 2, 2]}
+                align='center'
+                justify='space-between'
+              >
+                <Button variant='standard' w='100%' p={8}>
+                  Bytt passord
+                </Button>
+                <Button variant='standard' w='100%' p={8}>
+                  Endre
+                  <wbr /> adminstatus
+                </Button>
+              </Stack>
+            </Box>
+          </Box>
+        ))}
+      </Grid>
+    );
+  }
 
   return (
     <>
@@ -94,7 +204,7 @@ export default function UserAdminPage() {
           pb={[0, 0, 4, 4]}
           maxWidth='95vw'
         >
-          Brukeradministrasjon
+          Bruker&shy;administrasjon
         </Heading>
         <Box>
           <Box>
@@ -141,83 +251,7 @@ export default function UserAdminPage() {
         <Heading as='h2' size='xl' pt={8} mb={0} maxWidth='95vw'>
           Registrerte brukere
         </Heading>
-        <Grid
-          templateColumns={{
-            sm: 'repeat(1, 1fr)',
-            md: 'repeat(2, 1fr)',
-            lg: 'repeat(2, 1fr)',
-            xl: 'repeat(3, 1fr)',
-          }}
-          pt={8}
-          gap={4}
-          mb={4}
-          mt={0}
-        >
-          {sortedUsers.map((user) => (
-            <Box
-              key={user?.user_id}
-              minH={64}
-              minW='100%'
-              borderWidth='1px'
-              rounded='md'
-              shadow='lg'
-              bgColor='#eee'
-              overflow='hidden'
-            >
-              <Flex
-                m={8}
-                direction={['column', 'column', 'row', 'row']}
-                align={['center', 'center', 'left', 'left']}
-              >
-                <Image
-                  src={user?.picture}
-                  alt={user?.name}
-                  rounded='50%'
-                  width={32}
-                  mt={4}
-                  mx={8}
-                />
-                <Text as='div' fontSize='lg' fontWeight='semibold' align='left'>
-                  {user?.name}
-                  {user?.app_metadata?.Role ? (
-                    <Badge colorScheme='red'>ADMIN</Badge>
-                  ) : (
-                    ''
-                  )}
-                </Text>
-              </Flex>
-
-              <Box mx={8} mb={8}>
-                <Stack
-                  direction={['column', 'column', 'row', 'row']}
-                  align='center'
-                  justify='space-between'
-                >
-                  <Button variant='admin-button' w='100%' p={8}>
-                    Endre bruker
-                  </Button>
-                  <Button variant='danger' w='100%' p={8}>
-                    Slett bruker
-                  </Button>
-                </Stack>
-                <Stack
-                  direction={['column', 'column', 'row', 'row']}
-                  my={[2, 2, 2, 2]}
-                  align='center'
-                  justify='space-between'
-                >
-                  <Button variant='admin-button' w='100%' p={8}>
-                    Bytt passord
-                  </Button>
-                  <Button variant='admin-button' w='100%' p={8}>
-                    Endre
-                    <wbr /> adminstatus
-                  </Button>
-                </Stack>
-              </Box>
-            </Box>
-          ))}
-        </Grid>
+        {userList}
       </Box>
     </>
   );
