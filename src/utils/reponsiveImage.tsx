0993d20a964671f2a1384429b1ed7f3f71a6702a
Kurt Lekanger <kurt@lekanger.no>
2021-06-27 13:33:55 +0200

    refactor: make responsive image component

--- src/utils/reponsiveImage.tsx
+++ src/utils/reponsiveImage.tsx
@@ -1,38 +1,37 @@
+/**
+ *
+ *
+ *
+ */
+
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
 import * as React from 'react';
 
 interface ICustomImageProps {
-  src: string;
+  url: string;
   alt: string;
 }
 
-const ResponsiveImage = ({
-  src,
-  alt,
-  ...rest
-}: ICustomImageProps): JSX.Element => {
-  const small = `${src}?w=800&fm=webp 800w`;
-  const medium = `${src}?w=1600&fm=webp 1600w`;
-  const large = `${src}?w=1366&fm=webp 1366w`;
+// Breakpoints used by gatsby-plugin-image. I'll use the same...
+//  [750, 1080, 1366, 1920]
+
+const ResponsiveImage = ({ url, alt, ...rest }: ICustomImageProps) => {
+  const small = `${url}?w=750&&h=469&fit=fill&fm=webp 750w`;
+  const medium = `${url}?w=1080&&h=675&fit=fill&fm=webp 1080w`;
+  const large = `${url}?w=1366&h=854&fit=fill&&fm=webp 1366w`;
+  const xlarge = `${url}?w=1920&h=1200&fit=fill&&fm=webp 1920w`;
 
   return (
     <div>
       <picture>
-        <source
-          type='image/webp'
-          sizes='(max-width: 1200px) 1200px, 100vw'
-          src='//images.ctfassets.net/wxoemgzywng5/5r6WILLMKXX0qtACaRRVTm/ed394bffcfcc58531c8114b755de7d17/20210530_144100.jpg?w=1008&h=630&fl=progressive&q=50&fm=jpg'
-          srcSet=`` 
-        />
+        <source media='(min-width: 1920px)' type='image/webp' srcSet={xlarge} />
+        <source media='(min-width: 1366px)' type='image/webp' srcSet={large} />
+        <source media='(min-width: 1080px)' type='image/webp' srcSet={medium} />
+        <source type='image/webp' srcSet={small} />
         <img
-          src='//images.ctfassets.net/wxoemgzywng5/5r6WILLMKXX0qtACaRRVTm/ed394bffcfcc58531c8114b755de7d17/20210530_144100.jpg?w=1008&h=630&fl=progressive&q=50&fm=jpg'
-          alt=''
-          srcSet='
-        //images.ctfassets.net/wxoemgzywng5/5r6WILLMKXX0qtACaRRVTm/ed394bffcfcc58531c8114b755de7d17/20210530_144100.jpg?w=1008&h=630&fl=progressive&q=50&fm=jpg  1008w,
-        //images.ctfassets.net/wxoemgzywng5/5r6WILLMKXX0qtACaRRVTm/ed394bffcfcc58531c8114b755de7d17/20210530_144100.jpg?w=2016&h=1260&fl=progressive&q=50&fm=jpg 2016w,
-        //images.ctfassets.net/wxoemgzywng5/5r6WILLMKXX0qtACaRRVTm/ed394bffcfcc58531c8114b755de7d17/20210530_144100.jpg?w=4000&h=2500&fl=progressive&q=50&fm=jpg 4000w
-      '
           {...rest}
+          src={`${url}?w=1080&h=675&fit=fill&fl=progressive&q=50&fm=jpg`}
+          alt={alt}
         />
       </picture>
     </div>
