f35f20fe38152315eaa9b0392b0e7f7ca9d9ab67
Kurt Lekanger <kurt@lekanger.no>
2021-06-27 13:42:31 +0200

    fix: Appollo should be wrapped also in gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import { wrapPageElement as wrap } from './src/chakra-wrapper';
+import { ApolloWrapper } from './src/apollo/apollo-wrapper';
 
 // Wrap page element in Chakra Provider and Layout component
 export const wrapPageElement = wrap;
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloWrapper>{element}</ApolloWrapper>
+);
