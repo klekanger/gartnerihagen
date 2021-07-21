9af21de9dacca228b7da7a7bf1c6fbe44f1c7efc
Kurt Lekanger <kurt@lekanger.no>
2021-07-21 11:24:48 +0200

    feat: add custom hook for accessing protected APIs with access token

---
+++ src/components/hooks/useApi.js
@@ -0,0 +1,55 @@
+/**
+ * useApi hook for accessing protected APIs with an access token.
+ * https://github.com/auth0/auth0-react/blob/master/EXAMPLES.md#4-create-a-useapi-hook-for-accessing-protected-apis-with-an-access-token
+ *
+ * @param {string} url URL of the API you want to access
+ * @param {Object} options Fetch options, like audience and scope
+ *
+ */
+
+import { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+export const useApi = (url, options = {}) => {
+  const { getAccessTokenSilently } = useAuth0();
+  const [state, setState] = useState({
+    error: null,
+    loading: true,
+    data: null,
+  });
+  const [refreshIndex, setRefreshIndex] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { audience, scope, ...fetchOptions } = options;
+        const accessToken = await getAccessTokenSilently({ audience, scope });
+        const res = await fetch(url, {
+          ...fetchOptions,
+          headers: {
+            ...fetchOptions.headers,
+            // Add the Authorization header to the existing headers
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        setState({
+          ...state,
+          data: await res.json(),
+          error: null,
+          loading: false,
+        });
+      } catch (error) {
+        setState({
+          ...state,
+          error,
+          loading: false,
+        });
+      }
+    })();
+  }, [refreshIndex]);
+
+  return {
+    ...state,
+    refresh: () => setRefreshIndex(refreshIndex + 1),
+  };
+};
