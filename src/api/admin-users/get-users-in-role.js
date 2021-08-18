053c353029d085382de12640e2d35b17f66c23dc
Kurt Lekanger <kurt@lekanger.no>
2021-08-18 20:16:49 +0200

    build: debugging Gatsby  functions on Netlify

--- src/api/admin-users/get-users-in-role.js
+++ src/api/admin-users/get-users-in-role.js
@@ -1,20 +1,19 @@
 /**
  * Returns a list of users and roles for each user.
  */
+const ManagementClient = require('auth0').ManagementClient;
+const {
+  JwtVerifier,
+  JwtVerifierError,
+  getTokenFromHeader,
+} = require('@serverless-jwt/jwt-verifier');
+
+const jwt = new JwtVerifier({
+  issuer: `https://${process.env.AUTH0_BACKEND_DOMAIN}/`,
+  audience: `https://${process.env.AUTH0_USERADMIN_AUDIENCE}`,
+});
 
 export default async function handler(req, res) {
-  const ManagementClient = require('auth0').ManagementClient;
-  const {
-    JwtVerifier,
-    JwtVerifierError,
-    getTokenFromHeader,
-  } = require('@serverless-jwt/jwt-verifier');
-
-  const jwt = new JwtVerifier({
-    issuer: `https://${process.env.AUTH0_BACKEND_DOMAIN}/`,
-    audience: `https://${process.env.AUTH0_USERADMIN_AUDIENCE}`,
-  });
-
   let claims, permissions;
   const token = getTokenFromHeader(req.get('authorization'));
 
