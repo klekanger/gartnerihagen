f5f1e0d376ed24ca3e3aaf7338851c722b94dcef
Kurt Lekanger <kurt@lekanger.no>
2021-08-18 20:19:36 +0200

    build: debug Gatsby functions on Netlify

--- src/api/admin-users/test.js
+++ src/api/admin-users/test.js
@@ -12,5 +12,42 @@ const jwt = new JwtVerifier({
 });
 
 export default function handler(req, res) {
+  let claims, permissions;
+  const token = getTokenFromHeader(req.get('authorization'));
+
+  // Verify access token
+  try {
+    claims = await jwt.verifyAccessToken(token);
+    permissions = claims?.permissions ?? [];
+  } catch (err) {
+    if (err instanceof JwtVerifierError) {
+      return res.status(403).json({
+        error: `Something went wrong. ${err?.code}`,
+        error_description: `${err.message}`,
+      });
+    }
+  }
+
+  // check if user should have access at all
+  if (!claims || !claims?.scope) {
+    return res.status(403).json({
+      error: 'access denied',
+      error_description: 'You do not have access to this',
+    });
+  }
+
+  // Check the permissions
+  if (!permissions.includes('read:roles')) {
+    return res.status(403).json({
+      error: 'no read access',
+      status_code: res.statusCode,
+      error_description:
+        'Du må ha admin-tilgang for å administrere brukere. Ta kontakt med styret.',
+      body: {
+        data: [],
+      },
+    });
+  }
+
   res.status(200).json({ hello: `world` });
 }
