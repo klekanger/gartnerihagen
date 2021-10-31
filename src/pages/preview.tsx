12f648c0845cc355f36ed176b2911929f2e378d8
Kurt Lekanger <kurt@lekanger.no>
2021-10-31 10:32:21 +0100

    feat: add functionality for Contentful CMS previews

--- src/pages/preview.tsx
+++ src/pages/preview.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Router } from '@reach/router';
+import PreviewBlog from '../components/preview-blog';
 import PreviewPage from '../components/preview-page';
 
 export default function Preview() {
   return (
     <Router>
-      <PreviewPage path='/preview/:id' />
+      <PreviewBlog path='/preview/blog/:id' />
+      <PreviewPage path='/preview/page/:id' />
     </Router>
   );
 }
