61abba90314c5abc02fdb6312308af16032d6b7c
Kurt Lekanger <kurt@lekanger.no>
2022-07-24 17:40:31 +0200

    style: reduce maxwidth on laaaarge screens

--- src/components/layouts/container.tsx
+++ src/components/layouts/container.tsx
@@ -1,11 +1,16 @@
 // Default container for default page width
 // Wrap articles etc. in this one and everything should be nice and centered with the same width
-
 import { Box } from '@chakra-ui/react';
+import * as React from 'react';
 
 export default function Container({ children, ...rest }) {
   return (
-    <Box maxWidth={['97%', '95%', '95%', '70%']} pt={4} m='auto' {...rest}>
+    <Box
+      maxWidth={['97%', '95%', '95%', '70%', '50%']}
+      pt={4}
+      m='auto'
+      {...rest}
+    >
       {children}
     </Box>
   );
